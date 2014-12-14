8efe10d893be5fd9bc75f88298b6d638b707ca32
Samantha Nakis <samantha.nakis@gmail.com>
2014-12-14 16:49:34 -0500

    got rid of all modals and added links instead

--- app/views/food_index.blade.php
+++ app/views/food_index.blade.php
@@ -10,182 +10,68 @@
 <br> <br> <br> <br>
 
 <div class="container">
-	<a href="#" class="btn btn-primary btn-xs pull-right" data-toggle="modal" data-target="#new_entry"><b>+</b> Add more items</a>
-	<div class="row">
+	<a href="#" class="btn btn-primary btn-xs pull-right" type="submit" data-title="add_new"  onclick="window.location='/food/create';"><b>+</b> Add more items</a>
+<div class="row">
 		
         
-        <div class="col-md-12">
-        <h4>Family Shopping List</h4>
-        <div class="table-responsive">
+<div class="col-md-12">
+  <h4>Family Shopping List</h4>
+  <div class="table-responsive">
+    <table id="mytable" class="table table-bordred table-striped">
+                   
+      <thead>                   
+       <th><input type="checkbox" id="checkall" /></th>
+       <th>Item</th>
+       <th>Shopper</th>
+       <th>Store</th>
+       <th>Units</th>
+       <th>Amount</th>
+       <th>Purchased?</th>
+       <th>Edit</th>
+       <th>Delete</th>
+     </thead>
 
-                
-              <table id="mytable" class="table table-bordred table-striped">
-                   
-                   <thead>
-                   
-                     <th><input type="checkbox" id="checkall" /></th>
-                     <th>Item</th>
-                     <th>Shopper</th>
-                     <th>Store</th>
-                     <th>Units</th>
-                     <th>Amount</th>
-                     <th>Purchased?</th>
-                     <th>Edit</th>
-                     <th>Delete</th>
-                   </thead>
     <tbody>
     
-    @if(sizeof($foods) == 0)
-    	<h1>No results</h1>
+      @if(sizeof($foods) == 0)
+      	<h1>No results</h1>
 
-    @else
-    	@foreach($foods as $food)
-    	  <tr id={{$food['id']}}>
-		    <td><input type="checkbox" class="checkthis" /></td>
-		    <td><a href='/food/{{ $food['id']}}'>{{$food['food_type']}}</a></td>
-		    <td><a href='/person/{{$food['person']->id}}'>{{$food['person']->name}}</a></td>
-		    <td><a href='/store/{{$food['store']->id}}'>{{$food['store']->store_name}}</td>
-        <td>{{$food['units']}}</td>
-        <td>{{$food['amount']}}</td>
-        <td>
-        <?php
-          if($food['purchased'] == 1){
-            echo "yes";
-          }
-          else{
-            echo "no";
-          }
-        ?>
-      </td>
+      @else
+      	@foreach($foods as $food)
+      	  <tr id={{$food['id']}}>
+  		    <td><input type="checkbox" class="checkthis" /></td>
+  		    <td><a href='/food/{{ $food['id']}}'>{{$food['food_type']}}</a></td>
+  		    <td><a href='/person/{{$food['person']->id}}'>{{$food['person']->name}}</a></td>
+  		    <td><a href='/store/{{$food['store']->id}}'>{{$food['store']->store_name}}</td>
+          <td>{{$food['units']}}</td>
+          <td>{{$food['amount']}}</td>
+          <td>
+            <?php
+              if($food['purchased'] == 1){
+                echo "yes";
+              }
+              else{
+                echo "no";
+              }
+            ?>
+          </td>
 
-		    <td><p><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-        <td><p><button class="btn btn-danger btn-xs" type="submit" data-title="Delete" onclick="window.location='/food/{{$food['id']}}';"><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-<!--		    <td><p><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td> -->
-		    </tr>
-		@endforeach
-    @endif
+  		    <td><p><button class="btn btn-primary btn-xs" type="submit" data-title="Edit"  onclick="window.location='/food/{{$food['id']}}/edit';" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+          {{ Form::open(['method' => 'DELETE', 'action' => ['FoodController@destroy', $food->id]]) }}
+            <td><p><button class="btn btn-danger btn-xs" data-title="Delete"><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+          {{ Form::close() }}
+  		    </tr>
+		    @endforeach
+      @endif
 
     </tbody>
         
-</table>
+  </table>
                 
-            </div>
-            
-        </div>
-	</div>
+</div>            
 </div>
-
-<div class="modal fade" id="new_entry" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
-    <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title custom_align" id="Heading">Add Your Item</h4>
-      </div>
-          <div class="modal-body">
-          <div class="form-group">
-        <input id= "food_type" name="food_type" class="form-control " type="text" placeholder="grocery item">
-        </div>
-        <div class="form-group">
-        
-        <input id= "units" name="units" class="form-control " type="text" placeholder="units">
-        </div>
-        <div class="form-group">
-
-        <input id= "amount" name="amount" class="form-control " type="number" placeholder="amount">
-        </div>
-        <div class="form-group">
-
-        Shopper:
-        {{Form::select('person_id', $persons)}}
-        </div>
-        <div class="form-group">
-
-        Store:
-        {{Form::select('store_id', $stores)}}
-        </div>
-        <div class="form-checkbox-group">
-
-        <label class="control-label" for="purchased">Purchased?</label>
-        <div class="controls">
-          <label class="checkbox" for="purchased-0">
-            <input type="checkbox" name="purchased" id="purchased-0" value="1">
-            Yes
-          </label>
-          <label class="checkbox" for="purchased-1">
-            <input type="checkbox" name="purchased" id="purchased-1" value="0">
-            No
-          </label>
-        </div>
-        </div>
-        <div class="form-group">          
-        </div>
-      </div>
-          <div class="modal-footer ">
-        <button type="button" class="btn btn-warning btn-lg" onclick="edit_food_item()" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-      </div>
-        </div>
-    <!-- /.modal-content --> 
-  </div>
-      <!-- /.modal-dialog --> 
 </div>
-
-
-
-<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
-    <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
-      </div>
-          <div class="modal-body">
-          <div class="form-group">
-        <input class="form-control " type="text" placeholder="Mohsin">
-        </div>
-        <div class="form-group">
-        
-        <input class="form-control " type="text" placeholder="Irshad">
-        </div>
-        <div class="form-group">
-        <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
-    
-        
-        </div>
-      </div>
-          <div class="modal-footer ">
-        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-      </div>
-        </div>
-    <!-- /.modal-content --> 
-  </div>
-      <!-- /.modal-dialog --> 
-    </div>
-    
-    
-    
-    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
-    <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
-      </div>
-          <div class="modal-body">
-       
-       <div class="alert alert-warning"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this Record?</div>
-       
-      </div>
-        <div class="modal-footer ">
-
-        <button id="submit-modal" class="btn btn-success"><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
-      </div>
-        </div>
-    <!-- /.modal-content --> 
-  </div>
-      <!-- /.modal-dialog --> 
-    </div>
+</div>
 
 @stop
 
